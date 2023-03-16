6c19c8681d7c733a03a9d0d030971724037e1ba2
JohnnyNWT <jooaaooskt@gmail.com>
2023-03-16 02:51:30 -0300

    FEAT: cria endpoint get /talker/:id

--- src/routes/talkerRouter.js
+++ src/routes/talkerRouter.js
@@ -6,6 +6,14 @@ const talkerRouter = Router();
 talkerRouter.get('/', async (_req, res) => {
   const getAll = await readTalkerFile();
   return res.status(200).json(getAll);
+}).get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readTalkerFile();
+  const getById = talkers.filter(({ id: idTalker }) => idTalker === Number(id));
+  if (getById.length === 0) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return res.status(200).json(getById[0]);
 });
 
 module.exports = {
